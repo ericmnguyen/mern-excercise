b716598f845e66f694a0a60d91817519b2c07109
Nhat Nguyen <nhat.eric.nguyen@gmail.com>
2020-10-17 15:04:59 +0700

    fix

--- src/utils/appRequest.js
+++ src/utils/appRequest.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const basePath =
   process.env.NODE_ENV === 'production'
-    ? 'https://corona-tracking-api.herokuapp.com/'
+    ? 'https://corona-tracking-api.herokuapp.com'
     : 'http://localhost:5000';
 
 export function getAPI(url, params = {}, headers = {}) {
